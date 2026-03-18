c018c75b9c158065fdfd26a6174ccfb0ec2365e5
Bigjoe <bigjoefilm0@gmail.com>
2026-03-18 18:13:36 +0100

    this commit will fix font loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "streakservices",
+  title: "streakservice",
   description: "Streak LLC",
   icons: {
     icon: [{ url: "/icon", type: "image/png" }],
